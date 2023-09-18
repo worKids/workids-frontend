1c17a8d93b7fcce827b65c654cb4b57a52e0d00e
98noble <98noble@naver.com>
2023-09-18 14:18:24 +0900

    add : componets about citizen

---
+++ workids/src/components/teacher/citizen/TeacherImmigrantAcquire.jsx
@@ -0,0 +1,132 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function TeacherImmigrantAcquire() {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const navigate = useNavigate();
+  const join = () => {
+    axios
+      .post("http://localhost:8070/teacher/citizen/immigrant/acquire", {
+        id: id,
+        password: password,
+        name: name,
+        email: email,
+        phone: phone,
+      })
+      .then((response) => {
+        console.log(id);
+        console.log(password);
+        console.log(response);
+        navigate("/");
+      })
+      .catch((err) => {
+        alert(err.response.data.message);
+      });
+  };
+
+  const idHandler = (event) => {
+    setId(event.currentTarget.value);
+  };
+  const passwordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  };
+  const nameHandler = (event) => {
+    setName(event.currentTarget.value);
+  };
+
+  const emailHandler = (event) => {
+    setEmail(event.currentTarget.value);
+  };
+  const phoneHandler = (event) => {
+    setPhone(event.currentTarget.value);
+  };
+  return (
+    <div>
+      <div className="input-form-backgroud row ">
+        <div className="input-form col-md-12 mx-auto">
+          <div className="row justify-content-center">
+            <div className="mb-3 col-9 ">
+              <label htmlFor="name">아이디</label>
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                placeholder=""
+                value={id}
+                onChange={idHandler}
+              />
+            </div>
+          </div>
+          <div className="row justify-content-center">
+            <div className="mb-3 col-9 ">
+              <label htmlFor="nickname">비밀번호</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder=""
+                value={password}
+                onChange={passwordHandler}
+              />
+            </div>
+          </div>
+          <div className="row justify-content-center">
+            <div className="mb-3 col-9 ">
+              <label htmlFor="nickname">이름</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder=""
+                value={name}
+                onChange={nameHandler}
+              />
+            </div>
+          </div>
+          <div className="row justify-content-center">
+            <div className="mb-3 col-9 ">
+              <label htmlFor="nickname">이메일</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder=""
+                value={email}
+                onChange={emailHandler}
+              />
+            </div>
+          </div>
+          <div className="row justify-content-center">
+            <div className="mb-3 col-9 ">
+              <label htmlFor="nickname">휴대폰 번호</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder=""
+                value={phone}
+                onChange={phoneHandler}
+              />
+            </div>
+          </div>
+          <div className="invisible row justify-content-center">
+            <div className="mb-3 col-9 ">
+              <label htmlFor="nickname">휴대폰 번호</label>
+              <input type="text" className="form-control" placeholder="" />
+            </div>
+          </div>
+          <div className="row justify-content-center my-3">
+            <button
+              className="col-9 btn btn-warning btn-lg btn-block text-white"
+              type="submit"
+              onClick={join}
+            >
+              가입 완료
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
