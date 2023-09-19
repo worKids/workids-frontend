faeb6f5f6ea01f6b590e57576fe7514894af06db
98noble <98noble@naver.com>
2023-09-19 10:54:43 +0900

    add : components about job

---
+++ workids/src/components/teacher/job/TeacherJobInsert.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import Modal from 'react-bootstrap/Modal';
+import { useRecoilState } from "recoil";
+import { userState } from "../../../recoil/userAtoms";
+import { axBase } from "../../../apis/axiosInstance";
+
+export default function TeacherJobUpdate({citizenNumber, name}){
+    const [show, setShow] = useState(false);
+    const [userData, setUserData] = useRecoilState(userState);
+    
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+   
+
+
+    //직업부여 수정
+    const handleInsertJob = () => {
+        const token = userData.accessToken;
+        if (!token) {
+            navigate("/");
+        }
+    
+        axBase(token)({
+            method: "post",
+            url: "teacher/job/citizen/join",
+            data: {
+                nationNum: userData.nationNum,
+                citizenNumber : citizenNumber,
+                name : name,
+                state : 0
+            },
+        })
+        .then((response) => {
+            alert("직업 수정 완료");
+            handleClose();
+        })
+        .catch((err) => {
+            alert(err.response.data.message);
+        });
+    };
+
+
+    return(
+        <div>
+            <button onClick={handleShow}>등록</button>
+
+            <Modal show={show} onHide={handleClose}
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+            >
+                <Modal.Header>
+                    <Modal.Title>직업 등록하기</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div>
+                        정말로 등록하시겠습니까?
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <button onClick={() => handleInsertJob()}>Yes</button>
+                    <button onClick={handleClose}>No</button>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    )
+}
